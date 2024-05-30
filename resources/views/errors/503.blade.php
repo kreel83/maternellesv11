2f0b6671ec8f25749dd4c04ea283edb1ee8577fe
kreel83 <marc.borgna@gmail.com>
2024-05-30 23:01:16 +0200

    error + chatgpt

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,45 @@
-@extends('errors::minimal')
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #FC7A63;
+        color: white;
+        margin: 0;
+    }
+    .container {
+        text-align: center;
+    }
+    h1 {
+        font-size: 3em;
+        margin-bottom: 0.5em;
+    }
+    p {
+        font-size: 1.5em;
+        margin-bottom: 1em;
+    }
+    a {
+        text-decoration: none;
+        color: #007bff;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+    .error {
+        font-size: 120px;
+    }
+  
+</style>
+<div class="container">
+    <div style="display: flex; flex-direction: column; align-items: center">
+        <div class="error">503</div>
+        <hr>
+        <img src="{{asset('img/deco/logo.png')}}" alt="">        
+    </div>
 
-@section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __('Service Unavailable'))
+    <h1>Oops, quelque chose ne va pas !!!</h1>
+    <p>Désolé, un probleme est intervenu.</p>
+    <a href="{{ url('/app') }}">Retour au tableau de bord</a>
+</div>
